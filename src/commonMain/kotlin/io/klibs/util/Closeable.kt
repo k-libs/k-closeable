e30ef6a4d0f9f7e08255d4ba3a437de6524d2f5e
Elizabeth <foxcapade@gmail.com>
2022-11-20 18:26:58 -0500

    initial commit

---
+++ src/commonMain/kotlin/io/klibs/util/Closeable.kt
@@ -0,0 +1,19 @@
+package io.klibs.util
+
+/**
+ * # Closeable Resource
+ *
+ * Defines a type that wraps or implements a resource which may be closed.
+ *
+ * @author Elizabeth Paige Harper - https://github.com/foxcapades
+ * @since 1.0.0
+ */
+interface Closeable {
+
+  /**
+   * Closes this closable resource.
+   *
+   * Implementations of this method may throw exceptions.
+   */
+  fun close()
+}
